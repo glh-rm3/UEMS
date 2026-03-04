856a3ffa628c7ad554416832056606d73158b64b
Guito <gametguito@gmail.com>
2026-03-04 00:47:01 -0400

    Agora sim

--- Estrutura de dados 1/order_insert/main.c
+++ Estrutura de dados 1/order_insert/main.c
@@ -3,8 +3,7 @@
 
 #define MAX 100
 
-struct aluno{
-    
+struct aluno{    
     int matricula;
     char nome[30];
     float n1, n2, n3;
@@ -19,8 +18,7 @@ typedef struct lista Lista;
 
 Lista* cria_lista(){
     Lista *li = (Lista*) malloc(sizeof(Lista));
-    if(li != NULL)
-        li->qtd = 0;
+    if(li != NULL) li->qtd = 0;
     return li;
 }
 
@@ -50,39 +48,45 @@ int insere_final(Lista* li, struct aluno a){
 }
 
 int insere_ordenado(Lista* li, struct aluno a) {
-    if(li == NULL) puts("teste");
+    if(li == NULL) return 0;
     if(lista_cheia(li)) return 0;
     
-    int indice = 0;
-    for(int i = 0; i < li->qtd; i++) {
-        if(a.matricula > li->dados[i].matricula) {
-               indice = li->qtd;
-               printf("%d", indice);
-       }
-    } 
-    
-    for(int i = li->qtd; i < indice; i--) {
+    int indice, i;
+    i = indice = 0;
+    while(i < li->qtd && a.matricula > li->dados[i].matricula)
+        i++;
+     
+    indice = i;
+    for(int i = li->qtd; i > indice; i--) {
         li->dados[i] = li->dados[i - 1];
-        li->dados[i] = a;
-        printf("%d", a.matricula);
-        li->qtd++;
     }
-    puts("debug");
+    li->dados[i] = a;
+    li->qtd++;
 } 
 
-void imprime() {
-    
+void imprime(Lista * li) {
+    for(size_t i = 0; i < li->qtd; i++) {
+        printf("Aluno: %s\t\t matricula: %d\t\t media %.2f\n", li->dados[i].nome, li->dados[i].matricula,
+               (li->dados[i].n1 + li->dados[i].n2 + li->dados[i].n3) / 3);
+    }
 }
 
 int main() {
     Lista *li = cria_lista();
-    struct aluno a1 = {123, "Joao Silva", 8.5, 7.0, 9.0};
-    struct aluno a2 = {456, "Maria Santos", 9.0, 8.5, 7.5};
-    struct aluno a3 = {789, "Beta Santos", 9.0, 8.5, 7.5};
-    struct aluno a4 = {101, "Red Santos", 9.0, 8.5, 7.5};
-    struct aluno a5 = {411, "Pil Santos", 9.0, 8.5, 7.5};
+    struct aluno a1 = {123, "Willian Bonner", 8.5, 7.0, 9.0};
+    struct aluno a2 = {456, "Maria Gadu", 10.0, 5.5, 3.5};
+    struct aluno a3 = {789, "Beta User", 2.0, 4.5, 5.5};
+    struct aluno a4 = {101, "Red Pill aaaa", 6.0, 8.5, 8.5};
+    struct aluno a5 = {411, "Renan Santos", 1.0, 6.5, 6.5};
 
+    insere_ordenado(li, a1);
+    insere_ordenado(li, a2);
+    insere_ordenado(li, a3);
     insere_ordenado(li, a4);
-    
+    insere_ordenado(li, a5);
+
+    imprime(li);
+    printf("\nTamanho da lista: %d\n", tamanho_lista(li));    
+    return 0;
 }
 
