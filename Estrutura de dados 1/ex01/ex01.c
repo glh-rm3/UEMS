27d04aa098a6792485b2a14c92360aecaaaa1b42
Glh-Rm3 <157922178+glh-rm3@users.noreply.github.com>
2026-03-07 13:21:37 -0400

    style

--- Estrutura de dados 1/ex01/ex01.c
+++ Estrutura de dados 1/ex01/ex01.c
@@ -1,21 +1,28 @@
+/*
+Nome: Guilherme Henrique Ferreira de Oliveira 
+Matrícula: 54883
+Lista: 1
+Exercício: 1
+*/
+
 #include <stdio.h>
 
-int main() {
-  int x = 10, y = 20;
-  int *p1;
-  int *p2;
+int 
+main() {
+    int x = 10, y = 20;
+    int *p1;
+    int *p2;
 
-  printf("End de x: %p\n", &x);
-  printf("End de y: %p\n", &y);
+    printf("End de x: %p\n", &x);
+    printf("End de y: %p\n", &y);
 
-  p1 = &x;
-  p2 = &y;
-  printf("End de p1: %p\n", p1); 
-  printf("End de p2: %p\n", p2);
+    p1 = &x;
+    p2 = &y;
+    printf("End de p1: %p\n", p1); 
+    printf("End de p2: %p\n", p2);
+    (p1)++;
+    printf("End de p1: %p\n", p1); 
+    printf("End de p2: %p\n", p2);
 
-  (p1)++;
-  printf("End de p1: %p\n", p1); 
-  printf("End de p2: %p\n", p2);
-
-  return 0;
+    return 0;
 }
