d7a75b5f4ac0884c916673c3c896e37ee99ce28e
Glh-Rm3 <157922178+glh-rm3@users.noreply.github.com>
2026-03-07 13:31:41 -0400

    style

--- Estrutura de dados 1/ex03/ex03.c
+++ Estrutura de dados 1/ex03/ex03.c
@@ -16,11 +16,9 @@ main() {
 	int i = 750, j = 400, *aux;
 	printf("%p = %d\n%p = %d\n", &i, i, &j, j);
   
-    if(&j > &i) {
-        aux = &j;
-    } else {
-        aux = &i;
-    }
+    if(&j > &i) aux = &j;
+	else aux = &i;
+		
     printf("%p = %d\n", aux, *aux);
     return 0;
 }
