5fb7996d4d6d48e40726352143a5f8929ea47a7d
Glh-Rm3 <157922178+glh-rm3@users.noreply.github.com>
2026-04-28 14:00:57 -0400

    Refactor list functions and improve comments
    Refactored list implementation for clarity and added comments to improve code understanding.

--- ED1/L3/ex15/questao15.c
+++ ED1/L3/ex15/questao15.c
@@ -17,7 +17,7 @@ typedef struct {
 typedef struct Element {
 	Content data;
 	struct Element *prox;
-} Elem, *List;
+} Elem, *List; // Item Elem para criar um nó, e ponteiro List
 
 List * create_list() {
 	List *li = malloc(sizeof(List));
@@ -26,26 +26,29 @@ List * create_list() {
 	return li;
 }
 
+// Insere no ínicio dinâmicamente
 int insert_begin(List *li, Content c) {
-	if(li == NULL) return 0;
-	Elem *node = malloc(sizeof(Elem));
-	if(node == NULL) return 0;
+	if(li == NULL) return 0; 
+	Elem *node = malloc(sizeof(Elem)); // cria um nó para guardar o item 
+	if(node == NULL) return 0; // verifica se o nó foi alocado corretamente 
 
-	node->data = c;
-	node->prox = (*li);
-	*li = node;
+	node->data = c; // nó data recebe Content 
+	node->prox = (*li);	 // o proximo do meu nó, é o antigo primeiro
+	*li = node; // então o primeiro item da lista, agora aponta para nó, pois *li recebe o endereço de node
 	return 1;
 }
 
+// Função para verificar o tamanho da lista
 int size_list(List *li) {
 	if(li == NULL) return 0;
-	int cont = 0;
-	Elem *node = *li;
-	while(node != NULL) {
-		cont++;
-		node = node->prox;
+	int cont = 0; // criado um contador
+	Elem *node = *li; // cria um nó apontando para o primeiro item da lista
+	
+	while(node != NULL) { // forma de "caminhar" na lista, até chegarmos no final, já que o prox do último elemento é NULL
+		cont++; // contador incrementa 
+		node = node->prox; // node caminha até o último elemento 
 	}
-	return cont;
+	return cont; // retorna o valor do contador 
 }
 
 void free_list(List *li) {
@@ -60,21 +63,23 @@ void free_list(List *li) {
 	}
 }
 
-int insert_vector(List *li, int vector[]) {
+// Função que insere itens da lista no vetor 
+int insert_vector(List *li, int *vector) {
 	if(li == NULL) return 0;
 	
 	int i = 0;
-	Elem *node = *li;
-	while(node != NULL) {
-		vector[i] = node->data.value;
-		node = node->prox;
-		i++;
+	Elem *node = *li; // cria um no apontando para a primeira posição da lista
+	while(node != NULL) { // percorrer toda a lista
+		vector[i] = node->data.value; // vector[i] = node->data.value, do nó atual 
+		node = node->prox; // nó caminha para o próximo para preenchermos o vetor 
+		i++; // incrementa i para não sobrescrever 
 	}
 	return 1;
 }
 
-void print_vector(int vector[], size_t TAM){
-	for(size_t i = TAM; i > 0; i--) {
+// impressão comum do vetor, apenas para não poluir mais o main
+void print_vector(int *vector, size_t TAM){
+	for(size_t i = TAM; i > 0; i--) { 
 		printf("%d\n", vector[i-1]);
 	}
 }
@@ -85,27 +90,22 @@ void FAILED_INSERT() {
 
 int main() {
 	List *li = create_list();
-	Content c0, c1, c2, c3;
-	c0.value = 135;	
-	c1.value = 235;	
-	c2.value = 335;	
-	c3.value = 435;	
 
-	if(!insert_begin(li, c0)) {
+	if(!insert_begin(li, (Content) {135})) {
 		FAILED_INSERT();
 	}
-	if(!insert_begin(li, c1)) {
+	if(!insert_begin(li, (Content) {235})) {
 		FAILED_INSERT();
 	}
-	if(!insert_begin(li, c2)) {
+	if(!insert_begin(li, (Content) {335})) {
 		FAILED_INSERT();
 	}
-	if(!insert_begin(li, c3)) {
+	if(!insert_begin(li, (Content) {435})) {
 		FAILED_INSERT();
 	}
 	
 	//puts("DEBUG");
-	size_t TAM = size_list(li);
+	size_t TAM = size_list(li); // atribui o tamanho da lista para o TAM, que será o tamanho do nosso vetor
 	int vector[TAM];
 	if(!insert_vector(li, vector)) {
 		FAILED_INSERT();
