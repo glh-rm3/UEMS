cddb602788f36edd4e9bc53cff8dcb1f1a599c1e
Glh-Rm3 <157922178+glh-rm3@users.noreply.github.com>
2026-04-28 13:27:22 -0400

    Refactor list operations and improve error handling

--- ED1/L3/ex06/questao6.c
+++ ED1/L3/ex06/questao6.c
@@ -15,111 +15,121 @@ Exercício: 6
 
 #define MAX 5
 
+// estrutura para o conteúdo da lista
 typedef struct {
 	int content_value;
 } Content;
 
+// estrutura para a lista
 typedef struct {
 	int qtd;
 	Content value[MAX];
 } List;
-	
+
+// estrutura para a lista concatenada
 typedef struct {
 	int qtd;
-	Content value[MAX*2];
+	Content value[MAX*2]; // quando concatenada, teremos duas lista em um espaço de memória, logo MAX*2
 } Full_list;
 
+// função de criação de lista 
 List * create_list() {
-	List *li = (List*) malloc(sizeof(List));
-	if(li != NULL) {
-		li->qtd = 0;
+	List *li = (List*) malloc(sizeof(List)); // aloca memória dinamicamente para a estrutura da lista
+	if(li != NULL) { 						 // verifica se a alocação foi bem-sucedida antes de acessar os dados 
+		li->qtd = 0; 						 // inicializa a quantidade de elementos em 0
 	}
-	return li;
+	return li;	// retorna o ponteiro para a lista criada
 }	
 
+// está cheia? 
+int is_full(List *li) {
+	if(li == NULL) return -1; 
+	return (li->qtd == MAX); // retorna 0 se for falso e 1 se verdadeiro 
+}
+
+// inserção de conteúdo na lista - Insere no ínicio 
 int insert_content(List *li, Content c) {
-	if(li == NULL) return 0;
-	size_t i;	
-	for(i = li->qtd; i > 0; i--) {
-		li->value[i] = li->value[i-1];
+	if(li == NULL || is_full(li)) return 0;  // se li == NULL ou estiver cheia.
+	
+	for(int i = li->qtd; i > 0; i--) { // seja uma lista [5] [9] [ ] [ ] [ ] [ ] ..., i = 2, enquanto i > 0, i decrementa
+		li->value[i] = li->value[i-1]; // li->value[2] = value[1], ficando [5] [9] [9]
+									   // li->value[1] = value[0], ficando [5] [5] [9]
+									   // para o loop, resultando em [5] [5] [9]
 	}
 	
-	li->value[i] = c;
-	li->qtd++;
+	li->value[0] = c; // insere o novo elemento na primeira posição
+	li->qtd++; 		  // incrementa o qtd
 	return 1;
 }
 
-Full_list * Concatenate_List(List * li_0, List * li_1) {
-	Full_list *fl = (Full_list *) malloc(sizeof(Full_list));
-	if(fl != NULL) {
-		fl->qtd = 0;
-	}
+// função de concatenação - talvez eu refatore isso 
+Full_list * Concatenate_List(List * li_0, List * li_1){
+	if(li_0 == NULL || li_1 == NULL) return NULL;
 
-	for(size_t i = 0; i < li_0->qtd; i++) {
-		fl->value[fl->qtd] = li_0->value[i];
-		fl->qtd++;
+	Full_list *fl = (Full_list *) malloc(sizeof(Full_list)); // já criei a lista concatenada aqui mesmo 
+	if(fl == NULL) return NULL;
+
+	fl->qtd = 0;
+	for(size_t i = 0; i < li_0->qtd; i++) {  // percorrer até li_0->qtd 
+		fl->value[fl->qtd] = li_0->value[i]; // fl->value[0] = li_0->value[0] e assim por diante
+		fl->qtd++; // qtd incrementa
 	}
 	
-	for(size_t j = 0; j < li_1->qtd; j++) {
-		fl->value[fl->qtd] = li_1->value[j];
-		fl->qtd++;
+	for(size_t j = 0; j < li_1->qtd; j++) { // percorrer até l1_->qtd
+		fl->value[fl->qtd] = li_1->value[j]; // mesma ideia do for de cima, mas qtd não inicia zerado; ele pega o caminho andado
+		fl->qtd++; // qtd incrementa 
 	}
 
-	return fl;
+	return fl; // retorna ponteiro de fl 
 }
 
+// função para impressão da lista concatenada 
 void printf_Full_list(Full_list *fl) {
 	if(fl == NULL) return;
 
 	printf("Full List:");
-	for(size_t i = 0; i < fl->qtd; i++) {
-		printf(" %d ", fl->value[i].content_value);
+	for(size_t i = 0; i < fl->qtd; i++) { // percorre todos os elementos de fl
+		printf(" %d ", fl->value[i].content_value); // impressão comum do tipo inteiro para o campo value
 	}
 	printf("\n");
 }
 
-void merror() {
-	puts("Memory Allocation Error!");
+void ierror() {
+	puts("null or full list!");
 }
 
 int main() {
-	List *li_0 = create_list();
+	// criando as duas listas 
+	List *li_0 = create_list(); 
 	List *li_1 = create_list();
 
-	Content c0;
-	Content c1;
-	Content c2;
-	Content c3;
-	Content c4;
-	Content c5;
-
-	c0.content_value = 5;
-	c1.content_value = 10;
-	c2.content_value = 15;
-	c3.content_value = 20;
-	c4.content_value = 25;
-	c5.content_value = 30;
+	// estes ifs são apenas para validar se teve memória. Sinceramente, preciso deixar isso mais elegante. 
 	
-	if(!insert_content(li_0, c0)) {
-		merror();
+	if(!insert_content(li_0, (Content){5})) {
+	    ierror();
 	}
-	if(!insert_content(li_0, c1)) {
-		merror();
-	}	
-	if(!insert_content(li_0, c2)) {
-		merror();
+	
+	if(!insert_content(li_0, (Content){10})) {
+	    ierror();
 	}
-	if(!insert_content(li_1, c3)) {
-		merror();
+	
+	if(!insert_content(li_0, (Content){15})) {
+	    ierror();
 	}
-	if(!insert_content(li_1, c4)) {
-		merror();
+	
+	if(!insert_content(li_1, (Content){20})) {
+	    ierror();
 	}
-	if(!insert_content(li_1, c5)) {
-		merror();
+	
+	if(!insert_content(li_1, (Content){25})) {
+	    ierror();
 	}
-
-	Full_list *fl = Concatenate_List(li_0, li_1);
-	printf_Full_list(fl);
+	
+	if(!insert_content(li_1, (Content){30})) {
+	    ierror();
+	}
+	
+	Full_list *fl = Concatenate_List(li_0, li_1); // chama a função de concatenação 
+	printf_Full_list(fl); // chama a função de impressão
 	return 0;
 }	
